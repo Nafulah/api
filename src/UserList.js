ffc5561a7d01358ecaf35dcf6737dde527fbb59b
Nafulah <hadelynephionah@gmail.com>
2026-08-21 21:37:52 +0300

    added comments

--- src/UserList.js
+++ src/UserList.js
@@ -2,7 +2,9 @@ import React, {useState, useEffect} from "react";
 import axios from "axios";
 
 function UserList(){
+    //Used useState to save data
 const [listOfUser, setListOfUsers] = useState([]);
+//used axios and useEffect to get data
 useEffect(() => {
     axios.get("https://jsonplaceholder.typicode.com/users")
     .then(response => {
@@ -11,12 +13,13 @@ useEffect(() => {
     .catch(error => {
             console.log("Error Fetching from Users", error);
     });
-}, []);
+}, []); //empty array = run once
 return(
 <div style={{
     padding: '20px', fontFamily: 'Arial', backgroundColor: 'blueviolet', color: 'white'
 }}>
 <h1> List of Users </h1>
+{/*Map list of users to display*/}
 <ul>
     {listOfUser.map(user => (
         <li key={user.id} style={{marginBottom: '10px'}}>
